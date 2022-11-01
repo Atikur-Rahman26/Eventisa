6f7a45046041b10f8d559d3645f4cf176bc3073d
Atikur-Rahman26 <atikh152@gmail.com>
2022-11-02 01:11:06 +0600

    Back End has developed Some features should be deployed fantastically 1. Forgot Password 2. Sign out 3. Loading Dialog 4. Navigation bar should be modified 5. in host home host name and host mail should be added

--- app/src/main/java/com/atik/eventisa/AppHome.kt
+++ app/src/main/java/com/atik/eventisa/AppHome.kt
@@ -4,6 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_app_home.*
 
 class AppHome : AppCompatActivity() {
@@ -22,8 +28,64 @@ class AppHome : AppCompatActivity() {
         }
 
         NormalUserButton.setOnClickListener{
-            val intent=Intent(this,Login::class.java)
-            startActivity(intent)
+            var currentUserLoggedIn=FirebaseAuth.getInstance().currentUser
+            var auth=FirebaseAuth.getInstance()
+
+            if(currentUserLoggedIn!=null&&auth.currentUser!!.isEmailVerified){
+                var ref=
+                    FirebaseFirestore.getInstance().collection("USERS").whereEqualTo("Email",auth.currentUser!!.email.toString()).get().addOnSuccessListener {
+                        it->
+                    if(it.isEmpty){
+
+                    }
+                    else{
+                        var UserName= auth?.currentUser?.email.toString()
+
+                        var dbref= FirebaseDatabase.getInstance().getReference("UsersTable")
+                        dbref.addListenerForSingleValueEvent(object : ValueEventListener {
+                            override fun onDataChange(snapshot: DataSnapshot) {
+                                for(datasnapshot in snapshot.children){
+                                    var string:String=datasnapshot.child("email").value.toString()
+                                    if(string.equals(UserName)){
+                                        Constants.username =datasnapshot.child("userName").value.toString()
+                                        break
+                                    }
+                                }
+
+                                val dbref=
+                                    FirebaseDatabase.getInstance().getReference("UsersTable").child(
+                                        Constants.username
+                                    )
+                                dbref.addListenerForSingleValueEvent(object : ValueEventListener {
+                                    override fun onDataChange(snapshot: DataSnapshot) {
+                                        Constants.phone=snapshot.child("phoneNumber").value.toString()
+                                        Constants.fname=snapshot.child("firstName").value.toString()
+                                        Constants.lname=snapshot.child("lastName").value.toString()
+                                        Constants.email=UserName
+                                        val intent=Intent(this@AppHome,UserProfileActivity::class.java)
+                                        intent.putExtra("email",UserName)
+                                        startActivity(intent)
+                                        finish()
+                                    }
+
+                                    override fun onCancelled(error: DatabaseError) {
+                                        TODO("Not yet implemented")
+                                    }
+                                })
+                            }
+
+                            override fun onCancelled(error: DatabaseError) {
+                                TODO("Not yet implemented")
+                            }
+                        })
+                    }
+                }
+
+            }
+            else {
+                val intent = Intent(this, Login::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
